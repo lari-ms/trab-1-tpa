56a9423f8ab812a2611fbba855f112d68d0d175a
LumaTavares <lumatavares@gmail.com>
2026-03-10 19:29:51 -0300

    .

--- ListasEmJava/src/App.java
+++ ListasEmJava/src/App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        //testando meia bocaaa
         ListaEncadeada lista = new ListaEncadeada();
         lista.adicionar(5);
         System.out.println(lista);
     }
 }
+ 
