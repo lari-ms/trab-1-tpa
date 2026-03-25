9d6314a87b85e47b2110f0a3b5e42203132211eb
Lari <lari.msantos@yahoo.com.br>
2026-03-25 13:26:02 -0300

    leitura implementada e testes feitos

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Biblioteca de Listas Encadeadas em Java com Generics
 
 Implementação de uma biblioteca de lista encadeada genérica em Java, com análise de complexidade matemática e empírica, comparando o desempenho com ArrayList e LinkedList do Java.
+
+link pro docs:
+https://docs.google.com/document/d/1s9j7zC113brwik7d8vEWFbzAqe7uH03om-F15QsihKU/edit?tab=t.0
--- src/parte2/domain/LeitorArquivos.java
+++ src/parte2/domain/LeitorArquivos.java
@@ -6,13 +6,17 @@ import colecao.IColecao;
 import src.ListaEncadeada;
 
 public class LeitorArquivos {
-    private static final String NOME_ARQUIVO = "testes/funcionarios100k.txt";
+    private static final String NOME_ARQUIVO = "testes/funcionarios400k.txt";
     
     public static void main(String[] args) {
     
         IColecao<Funcionario> funcionarios;
         funcionarios = new ListaEncadeada<Funcionario>();
         Funcionario func;
+        String nome;
+        float salario;
+        long cpf = 0;
+
         long inicio = System.nanoTime();
         try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
             int numRegistros = Integer.parseInt(reader.readLine().trim());
@@ -21,9 +25,9 @@ public class LeitorArquivos {
             String linha;
             while ((linha = reader.readLine()) != null) {
                 String[] partes = linha.split(";");
-                String nome = partes[0];
-                float salario = Float.parseFloat(partes[1]);
-                long cpf = Long.parseLong(partes[2]);
+                nome = partes[0];
+                salario = Float.parseFloat(partes[1]);
+                cpf = Long.parseLong(partes[2]);
             
                 //inserindo na lista
                 func = new Funcionario(nome, cpf, salario);
@@ -33,10 +37,38 @@ public class LeitorArquivos {
 
             //calculando tempo de processamento
             long fim = System.nanoTime();
-            long duracaoNanos = (fim - inicio);
-            double duracaoMillis = (double) duracaoNanos / 1000000.0;
-
+            double duracaoMillis = (double) (fim - inicio) / 1000000.0;
             System.out.println("tempo de leitura de arquivo e insercao na lista - " + duracaoMillis + "ms");
+            
+
+            //pesquisando ultimo funcionario
+            inicio = System.nanoTime();
+            Funcionario ultimo = funcionarios.pesquisar(new Funcionario("",cpf,0));
+            fim = System.nanoTime();
+            duracaoMillis = (double) (fim - inicio) / 1000000.0;
+            System.out.println("tempo de busca pelo último elemento da lista - " + duracaoMillis + "ms");
+
+            //pesquisando funcionario inexistente
+            inicio = System.nanoTime();
+            funcionarios.pesquisar(new Funcionario("",0,0));
+            fim = System.nanoTime();
+            duracaoMillis = (double) (fim - inicio) / 1000000.0;
+            System.out.println("tempo de busca por um elemento inexistente na lista - " + duracaoMillis + "ms");
+
+            //excluindo ultimo funcionario da lista
+            inicio = System.nanoTime();
+            funcionarios.remover(ultimo);
+            fim = System.nanoTime();
+            duracaoMillis = (double) (fim - inicio) / 1000000.0;
+            System.out.println("tempo de remocao do ultimo elemento da lista - " + duracaoMillis + "ms");
+
+            //imprimindo a qtd de funcionarios na lista
+            inicio = System.nanoTime();
+            funcionarios.quantidadeNos();
+            fim = System.nanoTime();
+            duracaoMillis = (double) (fim - inicio) / 1000000.0;
+            System.out.println("tempo de impressão da quantidade de elementos da lista - " + duracaoMillis + "ms");
+
         } catch (IOException e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         } catch (NumberFormatException e) {
